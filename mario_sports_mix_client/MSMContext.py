f97264b04156650c94ea9b6c17ac7336dc69964b
ElectroStarz <theactualgreenone@gmail.com>
2026-06-21 19:30:42 +0100

    Changed version number

--- mario_sports_mix_client/MSMContext.py
+++ mario_sports_mix_client/MSMContext.py
@@ -20,8 +20,8 @@ from .memory_addresses_pal import *
 from .common_address_library import AddressLib
 
 id_to_name = {data.id: name for name, data in item_table.items()}
-CLIENT_VERSION = "1.0.3"
-COMPATIBLE_VERSIONS = ["1.0.0", "1.0.1", "1.0.2"]
+CLIENT_VERSION = "1.0.4"
+COMPATIBLE_VERSIONS = ["1.0.0", "1.0.1", "1.0.2", "1.0.3"]
 
 
 status_messages = {
